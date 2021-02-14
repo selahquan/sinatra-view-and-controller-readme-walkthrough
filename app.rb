b01abe7a790694eaa491700a2ff6cb1430a09153
Josh <josh.handon@gmail.com>
2021-02-14 12:02:12 -0500

    commit tree 7ece42a8aca147f3d1e37edebe3d99fc16d82bde

--- app.rb
+++ app.rb
@@ -2,15 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
+    puts params
     erb :reverse
   end
 
   post '/reverse' do
-    erb :reversed
+    original_string = params["string"]
+  @reversed_string = original_string.reverse
   end
 
   get '/friends' do
     # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
   end
 end
